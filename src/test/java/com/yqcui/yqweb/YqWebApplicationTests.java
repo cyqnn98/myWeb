76b8cb2958a389b3cc519e58f54d39d59bcc39ea
yqcui <15833977395@163.com>
2023-06-19 16:45:17 +0100

    remove comments

--- src/test/java/com/yqcui/yqweb/YqWebApplicationTests.java
+++ src/test/java/com/yqcui/yqweb/YqWebApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.yqcui.yqweb;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class YqWebApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.yqcui.yqweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YqWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
